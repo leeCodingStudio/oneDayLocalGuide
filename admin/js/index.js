5efe07a05a9a010c27e23ef14f8b49146f030525
xhpduk <xhpduk@gmail.com>
2022-10-22 19:36:13 +0900

    Refactor : HTML 클래스 이름 변경 및 CSS 최적화
    HTMl5 Sementic Tag로 태그 교체
    - details, summary로 버튼다운 구현
    Responsive Web 형태로 CSS 적용
    - 높이, 넓이에서 px단위 제거

--- admin/js/index.js
+++ admin/js/index.js
@@ -1,18 +1,3 @@
-const NavMenu = document.querySelectorAll(".nav-menu > a");
-const NavSubMenu = document.getElementsByClassName("nav-sub-menu");
+const NavMenu = document.getElementsByClassName("nav-menu");
 
-console.log(NavMenu[1].innerHTML);
-console.log(NavSubMenu[0].innerHTML);
-
-NavMenu[1].addEventListener("click", function () {
-  console.log(this.style.display);
-});
-
-function dropDown() {
-  if (this.style.display === "none") {
-    this.style.display = "block";
-  }
-  if (this.style.display === "block") {
-    this.style.display = "none";
-  }
-}
+console.log(NavMenu[0]);
